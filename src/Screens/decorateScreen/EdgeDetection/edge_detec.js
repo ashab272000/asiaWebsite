3eef0b42efc1615c2db7fdd7c7d41b78d685c5da
Mohammed Ashab Uddin <mu15@hw.ac.uk>
2020-05-31 08:32:19 +0400

    Edge detect is completerd

--- src/Screens/decorateScreen/EdgeDetection/edge_detec.js
+++ src/Screens/decorateScreen/EdgeDetection/edge_detec.js
@@ -1,195 +1,171 @@
+import { ImageLayer } from "../Objects/image_layer";
 
 
-export class EdgeDetect{
 
-    constructor(canvas, colorConverter) {
+export class EdgeDetect extends ImageLayer{
+
+    constructor(src, canvasController, colorConverter) {
+        super(src, canvasController);
         this._canvas = canvas;
         this._colorConverter = colorConverter;
         //edge detect works by comparing each pixel with its neighbore
         //color experiments showed that if the color is too dark or too light
         //we must compare by saturation
         //else we compare with hue
-        this._saturationComparision = false;
-
-    }
-
-    detectEdge(imgData, mousePosition){
-
-        let pixelClicked = this._getPixelClicked;
-        this._checkSaturationComparision();
-
-    }
-
-    _getPixelClicked(){
-        return (mouseX + mouseY * _canvas.width);
-    }
-
-    _checkSaturationComparision(){
-        //check to configure the  variable
-        if(hslPixel[1] < 0.15 || hslPixel[1] > 0.85)
-        {
-            
-            this._checkSaturationComparision = true;
-        }else{
-            this._checkSaturationComparision = false;
+        this._direction = {
+            up : -this._canvas.width,
+            right : 1,
+            down : this._canvas.height,
+            left: -1
         }
+
+        this._comparision = {
+            hue: 0,
+            saturation: 1
+        }
+
     }
 
-//detect the edge
-    edgeDetect(mouseX, mouseY, imgData){
-        //this is the variable that holds which color variable to compare
-        //generaly it compares the hue of a color which is 0
-        //but white and black comparision needs to be compared by saturation
-        //lightness in hsl represents the shades, therefore we dont need to compare them.
-        //saturation < 0.1 = white
-        //saturation > 0.9 = black
-        //else compare with hue in hsl
-        //hsl[0] = hue
-        //hsl[1] = saturation
-        //hsl[2] = lightness
-        let compare = 0;
+    detectEdge(refImg, mouseX, mouseY){
 
-        //get the pixel number clicked
-        let pixel = this._getPixelClicked;
+        //get the pixel clicked
+        let pixelClicked = this._getPixelClicked(mouseX, mouseY);
 
         //get the hsl of the pixel
-        let hslPixel = rgbToHsl(imgData.data[pixel*4], imgData.data[pixel* 4 + 1], imgData.data[pixel * 4 + 2]);
+        let refPixelHsl = this._colorConverter.rgbToHsl(refImg.data[pixelClicked * 4], refImg.data[pixelClicked * 4 + 1],  refImg.data[pixelClicked * 4 + 2]);
 
+        // run the algorithim
+        this._detectEdgeViaBreadth(refImg, pixelClicked, this._getComparision(refPixelHsl));
+    }
 
-        //check to configure the compare variable
-        if(hslPixel[1] < 0.15 || hslPixel[1] > 0.85)
-        {
-            //saturation will be compared
-            compare = 1;
-        }else{
-            compare = 0;
-        }
+    _detectEdgeViaBreadth(refImg, pixel, comparision){
 
-        //detect the pixels
-        let info = pixelEdgeDetect(compare, pixel, imgData);
-
-        return [imgData, info[0], info[1], info[2], info[3]];
-
-    };
-
-    _pixelEdgeDetect (compare, pixel, imgData){
-
-        //these are needed for lighting purposes
-        let lowestS = 1000;
-        let highestS = 0;
-        let lowestL = 1000;
-        let highestL = 0;
-
-        imgData.data[pixel * 4] = 255;
-        imgData.data[pixel * 4 + 1] = 255;
-        imgData.data[pixel * 4 + 2] = 255;
-
-        let nextPixel = pixel;
-        let pixels=[];
+        this._pixelToWhite(pixel);
+        //Recursion reaches stack overflow
+        //therefore, using "Recursion" with while loop
+        let nextPixel;
+        let pixelsTraversed = []
         let pop = false;
-        
-        //check and change pixel above
+
         while (nextPixel != -1 && nextPixel !== undefined)
         {
             let currentPixel = nextPixel;
-            let res;
-            nextPixel = changePixelEdge(currentPixel, compare, imgData, -rect.width, pop);
-
+            nextPixel = this._getNextPixel(currentPixel, comparision, refImg, direction.up, pop);
             if(nextPixel != -1 && nextPixel !== undefined)
             {
-                res = runSuccessChangePixelEdge(imgData,nextPixel, currentPixel, lowestL, highestL, pixels);
-                lowestL = res[0];
-                highestL = res[1];
+                this._pixelToWhite(nextPixel);
+                pixelsTraversed.push(currentPixel);
                 pop = false;
-            }else{
-                nextPixel = changePixelEdge(currentPixel, compare, imgData, 1, pop);
-                if(nextPixel != -1 && nextPixel !== undefined)
-                {
-                    res = runSuccessChangePixelEdge(imgData,nextPixel, currentPixel, lowestL, highestL, pixels);
-                    lowestL = res[0];
-                    highestL = res[1];
-                    pop = false;
-                }else{
-                    nextPixel = changePixelEdge(currentPixel, compare, imgData, rect.width, pop);
-                    if(nextPixel != -1 && nextPixel !== undefined)
-                    {
-                        res = runSuccessChangePixelEdge(imgData,nextPixel, currentPixel, lowestL, highestL, pixels);
-                        lowestL = res[0];
-                        highestL = res[1];
-                        pop = false;
-                    }else{
-                        nextPixel = changePixelEdge(currentPixel, compare, imgData, -1, pop);
-                        if(nextPixel != -1 && nextPixel !== undefined)
-                        {
-                            res = runSuccessChangePixelEdge(imgData,nextPixel, currentPixel, lowestL, highestL, pixels);
-                            lowestL = res[0];
-                            highestL = res[1];
-                            pop = false;
-                        }else{
-                            nextPixel = pixels.pop();
-                            pop = true;
-                        } //else while loop ends here     
-                    }
-                }
+                continue;
             }
-
+            nextPixel = this._getNextPixel(currentPixel, comparision, refImg, direction.right, pop);
+            if(nextPixel != -1 && nextPixel !== undefined)
+            {
+                this._pixelToWhite(nextPixel);
+                pixelsTraversed.push(currentPixel);
+                pop = false;
+                continue;
+            }
+            nextPixel = this._getNextPixel(currentPixel, comparision, refImg, direction.down, pop);
+            if(nextPixel != -1 && nextPixel !== undefined)
+            {
+                this._pixelToWhite(nextPixel);
+                pixelsTraversed.push(currentPixel);
+                pop = false;
+                continue;
+            }
+            nextPixel = this._getNextPixel(currentPixel, comparision, refImg, direction.left, pop);
+            if(nextPixel != -1 && nextPixel !== undefined)
+            {
+                this._pixelToWhite(nextPixel);
+                pixelsTraversed.push(currentPixel);
+                pop = false;
+                continue;
+            }else{
+                nextPixel = pixels.pop();
+                pop = true;
+            } 
         }
 
-        return [lowestS, highestS, lowestL, highestL];
-
-
-    };
-
-    _runSuccessChangePixelEdge(imgData,nextPixel, currentPixel, lowestL, highestL,pixels){
-
-        edgeColorChange(imgData, nextPixel);
-        pixels.push(currentPixel);
-
-        let hsl = rgbToHsl(imgData.data[nextPixel], imgData.data[nextPixel + 1], imgData.data[nextPixel + 2]);
-
-        if(hsl[2] < lowestL)
-        {
-            lowestL = hsl[2];
-        }
-        if(hsl[2] > highestL)
-        {
-            highestL = hsl[2];
-        }
-        return [lowestL, highestL];
 
     }
 
+    
+    _pixelToWhite(pixel) {
+        this._imgData.data[pixel * 4] = 255;
+        this._imgData.data[pixel * 4 + 1] = 255;
+        this._imgData.data[pixel * 4 + 2] = 255;
+        this._imgData.data[pixel * 4 + 3] = 69;
+    }
 
-    _changePixelEdge(currentPixel, compare , imgData,value, pop){
+
+    _getPixelClicked(mouseX, mouseY){
+        return (mouseX + mouseY * _canvas.width);
+    }
+
+    _getComparision(pixel){
+        //check to configure the  variable
+        
+        if(pixel[1] < 0.15 || pixel[1] > 0.85)
+        {
+            return this._comparision.saturation;
+        }else{
+            return this._comparision.hue;
+        }
+
+    }
+
+    _getHslFromRgb(pixel, imgData){
+        return this._colorConverter.rgbToHsl(imgData.data[pixel * 4], imgData.data[pixel * 4 + 1], imgData.data[pixel * 4 + 2]);
+    }
+
+    _getRgbFromHsl(pixel, imgData){
+        return this._colorConverter.rgbToHsl(imgData.data[pixel * 4], imgData.data[pixel * 4 + 1], imgData.data[pixel * 4 + 2]);
+    }
+
+    _getRgb(pixel, imgData){
+        return [imgData.data[pixel * 4], imgData.data[pixel * 4 + 1], imgData.data[pixel * 4 + 2], imgData.data[pixel * 4 + 3]]
+    }
+
+    _getNextPixel(currentPixel, compare, refImg, direction, isPoped){
         //get the hsl of the pixel
-        let currentHsl = rgbToHsl(mainImgData.data[currentPixel * 4], mainImgData.data[currentPixel * 4 + 1], mainImgData.data[currentPixel * 4 + 2]);
+        let currentHsl = this._getHsl(currentPixel, refImg);
         let compareValue;
         let compareLightness;
         let comparePixel;
         let comparePixelHsl;
 
         //compare the pixel above
-        comparePixel = currentPixel + (value);
+        comparePixel = currentPixel + (direction);
 
-
-        if(!(imgData.data[comparePixel * 4] == 255 && imgData.data[comparePixel * 4 + 1] == 255  && imgData.data[comparePixel * 4 + 2] == 255 && imgData.data[comparePixel * 4 + 3] == 69) )
+        //get the rgb value of the pixel of the edgeimg layer
+        //check if the pixel is already part of the pixel
+        let currentPixelRgbEdge = _getRgb(currentPixel, this._imgData);
+        if(!(currentPixelRgbEdge[0] == 255 && currentPixelRgbEdge[1] == 255 && currentPixelRgbEdge[2] == 255 && currentPixelRgbEdge[3] == 69) )
         {
-            comparePixelHsl = rgbToHsl(mainImgData.data[comparePixel * 4], mainImgData.data[comparePixel * 4 + 1], mainImgData.data[comparePixel * 4 + 2]);
+            //get the hsl for the pixel to be compared in the mainimglayer
+            comparePixelHsl = this._getHslFromRgb(currentPixel, refImg);
             compareValue = Math.abs(currentHsl[compare] - comparePixelHsl[compare]);
+
+            //compare == 0 means comparison has to be done in hue
+            //compare == 1 means comparision has to be done in saturation
             if(compare == 0)
             {
                 compareValue /= 360;
             }
+
             compareLightness = currentHsl[2] - comparePixelHsl[2];
 
-            let currentRgbValue = [mainImgData.data[currentPixel * 4], mainImgData.data[currentPixel * 4 + 1], mainImgData.data[currentPixel * 4 + 2]];
-            let compareRgbValue = [mainImgData.data[comparePixel * 4], mainImgData.data[comparePixel * 4 + 1], mainImgData.data[comparePixel * 4 + 2]];
+            let currentRgbValue = this._getRgb(currentPixel, refImg);
+            let compareRgbValue = this._getRgb(comparePixel, refImg);
+
+            //change this
             let rgbDiff = Math.abs(((currentRgbValue[0] + currentRgbValue[1] + currentRgbValue[2])/3) - ((compareRgbValue[0] + compareRgbValue[1] + compareRgbValue[2])/3));
 
-            
             if( compareValue < 0.03 && compareLightness > -0.06 && compareLightness < 0.06 && rgbDiff < 5)
             {
-                if(!pop)
+                //this gets the avg values of lightness and saturation
+                if(!isPoped)
                 {
                     let compareHsl = rgbToHsl(...compareRgbValue);
                     avgL = (avgL + compareHsl[2])/2;
@@ -200,21 +176,8 @@ export class EdgeDetect{
                 return -1;
             }
 
-
-
-
         }else{
             return -1;
-        }
-
-    };
-
-    edgeColorChange(imgData, pixel)
-    {
-        imgData.data[pixel * 4] = 255;
-        imgData.data[pixel * 4 + 1] = 255;
-        imgData.data[pixel * 4 + 2] = 255;
-        imgData.data[pixel * 4 + 3] = 69; 
+        }    
     }
-
 }
